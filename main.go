c5555fa5d17334082575dbd0eb6854a4b988e366
Hugh <hugh@HughdeMacBook-Pro.local>
2021-06-20 23:27:29 +0800

    新增关于页面

--- main.go
+++ main.go
@@ -6,7 +6,13 @@ import (
 )
 
 func handleFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Hello，这里是goblog</h1>")
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "<h1>Hello，这里是goblog</h1>")
+	}else if r.URL.Path == "/about" {
+		fmt.Fprint(w, "about页面")
+	} else {
+		fmt.Fprint(w,"<h1>页面未找到:(</h1>")
+	}
 }
 
 func main() {
