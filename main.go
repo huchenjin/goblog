7799ef29b3b437a5f0cf8dda248178f58a184777
胡陈锦 <huchenjin@xmpaoyou.com>
2021-07-06 11:12:19 +0800

    重构中间件

--- main.go
+++ main.go
@@ -5,10 +5,10 @@ import (
 	"fmt"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
+	"goblog/app/http/middlewares"
 	"goblog/bootstrap"
 	"goblog/pkg/database"
 	"net/http"
-	"strings"
 )
 
 var router *mux.Router
@@ -18,27 +18,6 @@ var db *sql.DB
 type Article struct {
 	Title, Body string
 	ID          int64
-}
-
-func forceHTMLMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// 1. 设置标头
-		w.Header().Set("Content-Type", "text/html; charset=utf-8")
-		// 2. 继续处理请求
-		next.ServeHTTP(w, r)
-	})
-}
-
-func removeTrailingSlash(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// 1. 除首页以外，移除所有请求路径后面的斜杆
-		if r.URL.Path != "/" {
-			r.URL.Path = strings.TrimSuffix(r.URL.Path, "/")
-		}
-
-		// 2. 将请求传递下去
-		next.ServeHTTP(w, r)
-	})
 }
 
 func main() {
@@ -52,6 +31,6 @@ func main() {
 
 	homeURL, _ := router.Get("home").URL()
 	fmt.Println("homeURL: ", homeURL)
-	router.Use(forceHTMLMiddleware)
-	http.ListenAndServe(":3000", removeTrailingSlash(router))
+	router.Use(middlewares.ForceHTML)
+	http.ListenAndServe(":3000", middlewares.RemoveTrailingSlash(router))
 }
