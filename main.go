e69a24f9d17ad7a064a7e26e24fad91784301de8
胡陈锦 <huchenjin@xmpaoyou.com>
2021-07-06 11:33:41 +0800

    美化index

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"database/sql"
-	"fmt"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 	"goblog/app/http/middlewares"
@@ -14,12 +13,6 @@ import (
 var router *mux.Router
 var db *sql.DB
 
-// Article  对应一条文章数据
-type Article struct {
-	Title, Body string
-	ID          int64
-}
-
 func main() {
 	// 初始化mysql
 	database.Initialize()
@@ -29,8 +22,5 @@ func main() {
 
 	router = bootstrap.SetupRoute()
 
-	homeURL, _ := router.Get("home").URL()
-	fmt.Println("homeURL: ", homeURL)
-	router.Use(middlewares.ForceHTML)
 	http.ListenAndServe(":3000", middlewares.RemoveTrailingSlash(router))
 }
