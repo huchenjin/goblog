a61ea12bba357c7ccc3aae71b40b5024191bc1d3
胡陈锦 <huchenjin@xmpaoyou.com>
2021-07-21 17:40:43 +0800

    会话和认证管理

--- app/models/user/crud.go
+++ app/models/user/crud.go
@@ -3,13 +3,32 @@ package user
 import (
 	"goblog/pkg/logger"
 	"goblog/pkg/model"
+	"goblog/pkg/types"
 )
 
-func (user *User) Create() (err error) {
-	if err = model.DB.Create(&user).Error; err != nil {
+func (u *User) Create() (err error) {
+	if err = model.DB.Create(&u).Error; err != nil {
 		logger.LogError(err)
 		return err
 	}
 
 	return nil
+}
+
+func Get(uid string) (User, error) {
+	var user User
+	id := types.StringToInt(uid)
+	if err := model.DB.First(&user, id).Error; err != nil {
+		return user, err
+	}
+
+	return user, nil
+}
+
+func GetByEmail(email string) (User, error) {
+	var user User
+	if err := model.DB.Where("email = ?", email).Find(&user).Error; err != nil {
+		return user, err
+	}
+	return user, nil
 }
